8da294aadb30d49f34c27e07190ae9fc49064342
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-02-09 16:24:00 +0100

    use quarto from nix
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,26 +1,40 @@
 {
   description = "My slides";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    mynur.url = "github:sagikazarmark/nur-packages";
+    mynur.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, mynur }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [
+            (final: prev: {
+              quarto = mynur.packages.${system}.quarto;
+            })
+          ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            # quarto
+            quarto
+            netlify-cli
 
-            python310Packages.jupyter
-            python310Packages.matplotlib
-            python310Packages.plotly
+            # python310Packages.jupyter
+            # python310Packages.matplotlib
+            # python310Packages.plotly
           ];
 
           shellHook = ''
-            echo -e "Welcome to the developer console!\n"
+            echo "quarto $(quarto --version)"
+            netlify --version
           '';
         };
       });
