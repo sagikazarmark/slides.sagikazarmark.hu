43bada3541225c316bd3a247125e1fffda2a0660
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-11-07 08:58:50 -0600

    install decktape
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     devenv.url = "github:cachix/devenv";
+    mynur.url = "github:sagikazarmark/nur-packages";
+    mynur.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ flake-parts, ... }:
@@ -25,6 +27,8 @@
               # python310Packages.jupyter
               # python310Packages.matplotlib
               # python310Packages.plotly
+            ] ++ [
+              inputs'.mynur.packages.decktape
             ];
 
             scripts = {
